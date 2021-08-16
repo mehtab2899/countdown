b6c3d087c4808e9359336f268ba91a22bf68602f
mehtab2899 <mehtabmultani51@gmail.com>
2021-08-16 12:07:25 +0530

    finish project

---
+++ src/app.js
@@ -0,0 +1,33 @@
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minutesEl = document.getElementById("minutes");
+("months");
+const secondsEl = document.getElementById("seconds");
+
+const formatTime = (time) => {
+  return time < 10 ? `0${time}` : time;
+};
+
+const countdownFun = () => {
+  let countDownDate = new Date("28 oct 2021");
+  let currentDate = new Date();
+  console.log(countDownDate, currentDate);
+
+  const totalSeconds = (countDownDate - currentDate) / 1000;
+
+  const days = Math.floor(totalSeconds / 3600 / 24);
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const minutes = Math.floor(totalSeconds / 60) % 60;
+  const seconds = Math.floor(totalSeconds) % 60;
+
+  daysEl.innerHTML = formatTime(days);
+  hoursEl.innerHTML = formatTime(hours);
+  minutesEl.innerHTML = formatTime(minutes);
+  secondsEl.innerHTML = formatTime(seconds);
+};
+
+countdownFun();
+
+setInterval(() => {
+  countdownFun();
+}, 1000);
